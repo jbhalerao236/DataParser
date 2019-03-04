557496741e820f686be4efbae752752cd9ac9309
jessicabhalerao <jessicabhalerao@gmail.com>
2019-03-04 00:32:49 -0800

    create ArrayList of results and print list

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /***
  * Main class for data parser
  * Author: Jessica Bhalerao
@@ -7,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         //Test of utils
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
-        System.out.println(data);
+        ArrayList<ElectionResult> results = Utils.parse2016ElectionResults(data);
+        System.out.println(results);
     }
 }
