81880892306248dfbb40ea14afcd324009336a22
jessicabhalerao <jessicabhalerao@gmail.com>
2019-03-05 18:51:09 -0800

    fixed parse2016ElectionResults method

--- src/Utils.java
+++ src/Utils.java
@@ -1,14 +1,16 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Utils {
+import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.length;
 
-    public static String readFileAsString(String filePath){
+public class Utils {
+    public static String readFileAsString(String filepath) {
         StringBuilder output = new StringBuilder();
 
-        try(Scanner scanner = new Scanner(new File(filePath))){
+        try (Scanner scanner = new Scanner(new File(filepath))) {
 
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
@@ -17,34 +19,50 @@ public class Utils {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return output.toString();
     }
 
     public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
-        ArrayList<ElectionResult> results = new ArrayList<>();
+        ArrayList<ElectionResult> result = new ArrayList<>();
         String[] rows = data.split("\n");
-            for (int a = 1; a < rows.length; a++) {
-                String[] valuesArray = rows[a].split(",");
-                //votes_dem,votes_gop,total_votes,per_dem,per_gop,
-                //diff,per_point_diff,state_abbr,county_name,combined_fips
-                double votes_dem = Double.parseDouble(valuesArray[1]);
-                double votes_gop = Double.parseDouble(valuesArray[2]);
-                double total_votes = Double.parseDouble(valuesArray[3]);
-                double per_dem = Double.parseDouble(valuesArray[4]);
-                double per_gop = Double.parseDouble(valuesArray[5]);
-                //.replaceAll("\"", "") ???
-                //String diffString = valuesArray[6].replaceAll(",", "");
-                int index = valuesArray[6].indexOf("\"");
-                double diff = Double.parseDouble(valuesArray[6].substring(index + 1, valuesArray[6].length() - 2));
-                double per_point_diff = Double.parseDouble(valuesArray[7].replaceAll("%", ""));
-                String state_abbr = valuesArray[8];
-                String county_name = valuesArray[9];
-                double combined_fips = Double.parseDouble(valuesArray[10]);
-            ElectionResult electionResultObj = new ElectionResult(votes_dem,votes_gop,total_votes,
-                    per_dem,per_gop,diff,per_point_diff,combined_fips,state_abbr,county_name);
-            results.add(electionResultObj);
-        }
-        return results;
-    }
+        String[] linesArray;
 
+        for (int i = 1; i < rows.length; i++) {
+            int indexOfFirstQuote = rows[i].indexOf("\"");
+            if(indexOfFirstQuote != -1) {
+                int indexOfSecondQuote = rows[i].indexOf("\"", indexOfFirstQuote + 1);
+                String originalString = rows[i].substring(indexOfFirstQuote, indexOfSecondQuote + 1);
+                String newString = originalString.replaceAll("\"", "").replaceAll(",", "");
+                rows[i] = rows[i].replace(originalString, newString);
+            }
+
+
+
+            linesArray = rows[i].split(",");
+
+
+            double votes_dem = Double.parseDouble(linesArray[1]);
+            double votes_gop = Double.parseDouble(linesArray[2]);
+            double total_votes = Double.parseDouble(linesArray[3]);
+            double per_dem = Double.parseDouble(linesArray[4]);
+            double per_gop = Double.parseDouble(linesArray[5]);
+            System.out.println(linesArray[6]);
+            //String diffString = rows[6].replaceAll(",", "").replaceAll("\"", ""); //replaceAll(",",linesArray[6]);
+            //System.out.println(diffString);
+            double diff = Double.parseDouble(linesArray[6]);
+            //String per_diff_String = linesArray[7].replaceAll("%", ""); //replaceAll("%",linesArray[7]);
+            double per_point_diff = Double.parseDouble(linesArray[7].replaceAll("%", ""));
+            String state_abbr = linesArray[8];
+            String county_name = linesArray[9];
+            double combined_fips = Double.parseDouble(linesArray[10]);
+            ElectionResult electionResultObject = new ElectionResult(votes_dem, votes_gop, total_votes, per_dem, per_gop, diff, per_point_diff, state_abbr, county_name, combined_fips);
+            result.add(electionResultObject);
+
+
+        }
+
+
+        return result;
+    }
 }
