4991745937211c6b725055ee2886256c63702fba
jessicabhalerao <jessicabhalerao@gmail.com>
2019-03-10 21:23:03 -0700

    created state class with constructors + getters + setters

---
+++ src/State.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class State {
+    private String name;
+    private List<County> counties;
+
+    public State(String name, List<County> counties) {
+        this.name = name;
+        this.counties = counties;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<County> getCounties() {
+        return counties;
+    }
+
+    public void setCounties(List<County> counties) {
+        this.counties = counties;
+    }
+}
